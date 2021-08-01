be0c9fcfb18b50da2ffbcab85f12b0ced4293757
LucasSiqueira568 <siqueiralucas-pro@outlook.com>
2021-08-01 15:40:15 -0300

    Arquivos frontend

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../Main'
 
 export default props =>
     <Main icon="home" title="Início"
-        subtitle="Cadastro de Usuários com React.">
+        subtitle="Cadastro de Amigos com React.">
         <div className="display-4">Bem Vindo!</div>
         <hr />
         <p>Este é um Sistema de Cadastro de Amizades, Onde voçê pode organizar uma Lista de seus Melhores amigos.</p>
